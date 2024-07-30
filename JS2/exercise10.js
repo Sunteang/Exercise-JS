cfa3e92b68e563a887c1d3a8f463578b46bcceee
Sunteang S <sereysunteang@gmail.com>
2024-07-30 17:56:33 +0700

    added change JS2

--- JS2/exercise10.js
+++ JS2/exercise10.js
@@ -1,16 +1,26 @@
 // Write a function that takes a non negative number as a parameter and returns the factorial of that parameter
-function factorial(num) {
-  if (num < 0) {
-    return "negative number";
-  } else if (num === 0 || num === 1) {
-    return 1;
-  } else {
-    return num * factorial(num - 1);
-  }
-}
+// function factorial(num) {
+//   if (num < 0) {
+//     return "negative number";
+//   } else if (num === 0 || num === 1) {
+//     return 1;
+//   } else {
+//     return num * factorial(num - 1);
+//   }
+// }
 
-console.log(factorial(5));
+// console.log(factorial(5));
+// console.log(factorial(3));
+// console.log(factorial(4));
+// console.log(factorial(2));
+// console.log(factorial(-2));
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+console.log(factorial(0));
 console.log(factorial(3));
 console.log(factorial(4));
-console.log(factorial(2));
-console.log(factorial(-2));
